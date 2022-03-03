38aa9102eff2e755a6012886b86eb2acebffe34d
itmdeveloper <choidasol95@gmail.com>
2022-03-03 11:24:36 +0900

    실습 5강까지 작업 -프로퍼티 파일 수정

---
+++ src/main/java/com/jojo/admin/boot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.jojo.admin.boot.config.auth.dto;
+
+import com.jojo.admin.boot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+}
